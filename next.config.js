3c77e3c34f49e37cedc65b21c85d8bb8eec3e379
ellawizzy2-ui <ellawizzy2@gmail.com>
2026-05-21 13:38:15 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  // Change 'nexflow' to your actual GitHub repo name
-  basePath: process.env.NODE_ENV === 'production' ? '/nexflow' : '',
-  images: { unoptimized: true },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
